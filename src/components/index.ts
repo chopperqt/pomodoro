acdcd05e870b52b426006385dc868e0e523d3839
chopperqt <ilua292010@gmail.com>
2023-05-02 09:18:26 +0300

    Исправление проблем со сборкой

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,5 @@
-import TimeModal from "./time-modal/TimeModal";
 import SideBar from "./side-bar/SideBar";
 import ModalContainer from "./modal-container/ModalContainer";
-import Counter from "./counter/Counter";
 import Pointer from "./pointer/Pointer";
 import Button from './button/Button'
 import Switcher from './switcher/Switcher'
@@ -9,7 +7,6 @@ import Checkbox from "./checkbox/Checkbox";
 import CounterV2 from "./counter-v2";
 
 export {
-	TimeModal,
 	SideBar,
 	ModalContainer,
 	Counter,
