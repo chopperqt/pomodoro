0554810ac6c127c41de5f2b992ee74e3227e587d
Chopperqt <ilua292010@gmail.com>
2025-06-28 22:28:47 +0300

    ci: добавлено поднятие версии в package.json

--- release.config.mjs
+++ release.config.mjs
@@ -5,6 +5,10 @@ export default {
   branches: ["release"],
   plugins: [
     "@semantic-release/github",
+    ['@semantic-release/npm',
+      {
+        npmPublish: false, // Отключает публикацию в npm, но версию в package.json обновит
+      }],
     ["@semantic-release/exec", {
       "prepareCmd": "node scripts/update-tauri-version.js ${nextRelease.version}"
     }],
