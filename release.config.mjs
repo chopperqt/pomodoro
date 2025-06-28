f413574187ca9eed5d8407fb1800a3579a8cf290
Chopperqt <ilua292010@gmail.com>
2025-06-28 22:40:13 +0300

    ci: улучшения CI

--- release.config.mjs
+++ release.config.mjs
@@ -4,11 +4,12 @@
 export default {
   branches: ["release"],
   plugins: [
-    "@semantic-release/github",
-    ['@semantic-release/npm',
-      {
-        npmPublish: false, // Отключает публикацию в npm, но версию в package.json обновит
-      }],
+    ["@semantic-release/github", {
+      labels: ["release"]
+    }],
+    ['@semantic-release/npm', {
+      npmPublish: false, // Отключает публикацию в npm, но версию в package.json обновит
+    }],
     ["@semantic-release/exec", {
       "prepareCmd": "node scripts/update-tauri-version.js ${nextRelease.version}"
     }],
